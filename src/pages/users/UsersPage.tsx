d96754738fe69efcd6e6530bcbf6c49f4e02f807
shadrix <postilga.dima@gmail.com>
2021-12-15 22:21:27 +0200

    added user info

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,5 +1,8 @@
 import { observer } from 'mobx-react'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
+import Pagination from '../../components/Pagination'
+import UserCard from '../../components/UserCard'
 import { useInjection } from '../../ioc/ioc.react'
 import ownTypes from '../../ioc/ownTypes'
 import UsersPageStore  from '../../stores/pages/UsersPageStore'
@@ -8,8 +11,31 @@ import UsersPageStore  from '../../stores/pages/UsersPageStore'
 const UsersPage = observer(() => {
   const store = useInjection<UsersPageStore>(ownTypes.usersPageStore);
   
+  useEffect(() => {
+    const getUser = async () => {
+      await store.init();
+    }
+    getUser()
+  }, [store])
+
   return (
-    <>{store}</>
+    <Container>
+      <Row className="justify-content-center">
+        {store.isLoading ? (
+          <Spinner animation="border" />
+        ) : (
+          <>
+            {store.users?.map((user, key) => (
+              <Col key={key} sm={6} md={4} lg={3} xl={2} className="mb-2 mt-2">
+                <UserCard user={user} />
+              </Col>
+            ))}
+          </>
+        )}
+
+      </Row>
+      <Pagination total={store.totalPages} active={store.currentPage} onChange={(val) => { store.changePage(val) }}/>
+    </Container>
   )
 });
 
--- src/stores/pages/UserPageStore.ts
+++ src/stores/pages/UserPageStore.ts
@@ -1,13 +1,42 @@
-import { injectable } from "inversify";
-import { makeObservable } from "mobx";
+import { inject, injectable } from "inversify";
+import { action, makeObservable, observable, runInAction } from "mobx";
+import ownTypes from "../../ioc/ownTypes";
+import type { User } from "../../models/User";
+import UserService from "../../services/UserService";
 
 @injectable()
 export default class UserPageStore {
 
+    @observable user : User | null = null;
+    @observable isLoading = false;
+    @observable error = '';
 
     constructor(   
+        @inject(ownTypes.userService) private readonly userService: UserService
    ) {
        makeObservable(this);
    }
+
+   @action
+    public init = async () => {
+        this.error = '';
+        try {
+            this.isLoading = true;
+            const result = await this.userService.getById(1);
+            runInAction(()=> {
+                this.user = {
+                    ...result
+                };
+            });
+            
+          } catch (e) {
+            if (e instanceof Error) {
+                this.error = e.message;
+            }
+          }
+          runInAction(()=> {
+            this.isLoading = false;
+        });
+    }
 
 }
